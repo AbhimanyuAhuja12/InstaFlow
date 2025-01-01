c0c6e957e3027379789b1f1599dd663fd8e6bd22
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2025-01-01 12:12:15 +0530

    clerk auth,landing page

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignUp } from '@clerk/nextjs'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div><SignUp/></div>
+  )
+}
+
+export default page;
---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const page = async(props: Props) => {
+    //sever action , onboard the user
+
+    // check if he is already onboarded (status 200 || 201)
+    // if onboarder , send him to their particular "dashboard/theirDynamicID"
+
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
