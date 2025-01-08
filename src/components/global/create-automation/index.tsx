6d28cdd72bd3f4f94c44fd0498cab67274549c2a
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2025-01-08 12:32:16 +0530

    added automations in the flow

--- src/components/global/create-automation/index.tsx
+++ src/components/global/create-automation/index.tsx
@@ -1,23 +1,23 @@
-// 'use client'
+'use client'
 
-// import { Button } from '@/components/ui/button'
-// import React, { useMemo } from 'react'
-// import Loader from '../loader'
-// import { AutomationDuoToneWhite } from '@/icons'
-// import { useCreateAutomation } from '@/hooks/use-automations'
-// import { v4 } from 'uuid'
+import { Button } from '@/components/ui/button'
+import React, { useMemo } from 'react'
+import Loader from '../loader'
+import { AutomationDuoToneWhite } from '@/icons'
+import { useCreateAutomation } from '@/hooks/use-automations'
+import { v4 } from 'uuid'
 
-// type Props = {}
+type Props = {}
 
-// const CreateAutomation = (props: Props) => {
+const CreateAutomation = (props: Props) => {
 //   const mutationId = useMemo(() => v4(), [])
 
 //   console.log(mutationId)
 //   const { isPending, mutate } = useCreateAutomation(mutationId)
 
-//   return (
-//     <Button
-//       className="lg:px-10 py-6 bg-gradient-to-br hover:opacity-80 text-white rounded-full from-[#3352CC] font-medium to-[#1C2D70]"
+  return (
+    <Button
+      className="lg:px-10 py-6 bg-gradient-to-br hover:opacity-80 text-white rounded-full from-[#3352CC] font-medium to-[#1C2D70]"
 //       onClick={() =>
 //         mutate({
 //           name: 'Untitled',
@@ -26,26 +26,18 @@
 //           keywords: [],
 //         })
 //       }
-//     >
-//       <Loader state={isPending}>
-//         <AutomationDuoToneWhite />
-//         <p className="lg:inline hidden">Create an Automation</p>
-//       </Loader>
-//     </Button>
-//   )
-// }
-
-// export default CreateAutomation
-
-
-import React from 'react'
-
-type Props = {}
-
-const index = (props: Props) => {
-  return (
-    <div>create-automation</div>
+    >
+      <Loader state={false}>
+        <AutomationDuoToneWhite />
+        <p className="lg:inline hidden">Create an Automation</p>
+      </Loader>
+    </Button>
   )
 }
 
-export default index
+export default CreateAutomation
+
+
+
+  // create automations server action functionality
+  
